bf34116335360d797ee9c66aa6754f3684218e24
Fernando Pulido <140780603+fernando-pulido@users.noreply.github.com>
2025-08-18 15:50:14 -0500

    feat: api consumption

---
+++ src/api/api.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+type Character = { id: number; name: string; image: string }
+
+const api = () => {
+  const [characters, setCharacters] = useState<Character[]>([])
+
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/character')
+      .then(res => setCharacters(res.data.results))
+      .catch(err => console.error(err))
+  }, [])
+
+  return (
+    <div>
+      <h2>Personajes Rick & Morty</h2>
+      {characters.map(patients => (
+        <div key={patients.id}>
+          <img src={patients.image} alt={patients.name} width={100} />
+          <p>{patients.name}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default api
